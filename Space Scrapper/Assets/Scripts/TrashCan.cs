5cd9232d066f84be7afbee821913d25f03ad50c6
v-ilan <ilanvol96@gmail.com>
2026-03-17 18:29:44 +0200

    fixed trach can

--- Space Scrapper/Assets/Scripts/TrashCan.cs
+++ Space Scrapper/Assets/Scripts/TrashCan.cs
@@ -1,13 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
 
 
 public class TrashCan : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if(other.GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRGrabInteractable>() != null)
+        if(other.GetComponent<XRGrabInteractable>() != null)
         {
             other.gameObject.SetActive(false);
         }
