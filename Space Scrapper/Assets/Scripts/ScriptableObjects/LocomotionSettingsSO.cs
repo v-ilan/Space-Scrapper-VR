cbc083a5d0a2ac2f55975ea37c4b0de64d9ccde2
v-ilan <ilanvol96@gmail.com>
2026-03-25 14:28:11 +0200

    update materials

---
+++ Space Scrapper/Assets/Scripts/ScriptableObjects/LocomotionSettingsSO.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LocomotionSettingsSO", menuName = "Scriptable Objects/LocomotionSettingsSO")]
+public class LocomotionSettingsSO : ScriptableObject
+{
+    
+}
