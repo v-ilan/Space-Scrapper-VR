2940de29523a72f40fa9617088f4a7d123806bf7
v-ilan <ilanvol96@gmail.com>
2026-03-26 21:35:31 +0200

    Update SceneLoaderCallback.cs

--- Space Scrapper/Assets/Scripts/SceneLoaderCallback.cs
+++ Space Scrapper/Assets/Scripts/SceneLoaderCallback.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LoaderCallback : MonoBehaviour
+public class SceneLoaderCallback : MonoBehaviour
 {
     private bool isFirstUpdate = true;
 
