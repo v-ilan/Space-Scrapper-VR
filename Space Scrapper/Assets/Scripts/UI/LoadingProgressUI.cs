ee444e153979422e875e45c54574e481f0f1760e
v-ilan <ilanvol96@gmail.com>
2026-03-27 16:50:50 +0300

    Update LoadingProgressUI.cs

--- Space Scrapper/Assets/Scripts/UI/LoadingProgressUI.cs
+++ Space Scrapper/Assets/Scripts/UI/LoadingProgressUI.cs
@@ -1,16 +1,50 @@
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System.Linq;
 
 public class LoadingProgressUI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("UI Elements")]
+    [SerializeField] private TextMeshProUGUI loadingText;
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private Slider progressBar;
+    [SerializeField] private Image fillImage;
+    //[SerializeField] private Image spinnerIcon;
+
+    [Header("Settings")]
+    [SerializeField] private float dotInterval = 1f; // Speed of the dots
+
+    private string _baseText = "Loading";
+    private float _dotTimer = 0;
+    private int _dotCount = 0;
+    private int _dotMax = 3;
+    //[SerializeField] private float rotationSpeed = 200f;
+
+    private void Update()
     {
+        float progress = SceneLoader.GetLoadingProgress();
+        // Update the bar
+        progressBar.value = progress;
+        fillImage.fillAmount = progress;
+        //*/
+
+        /* Rotate the spinner (so they know the app is "Alive")
+            spinnerIcon.transform.Rotate(Vector3.forward, -rotationSpeed * Time.deltaTime);
+        */
+
+        UpdateLoadingDots();
+    }
+    private void UpdateLoadingDots()
+    {
+        _dotTimer += Time.deltaTime;
         
+        if(_dotTimer >= dotInterval)
+        {
+            _dotCount = (_dotCount + 1) % _dotMax + 1;
+
+            loadingText.text = _baseText + string.Concat(Enumerable.Repeat(".", _dotCount));
+        }
+
     }
 }
