ef05eeb5ecdbe2faa7e552ab2156a6cce3c6226b
v-ilan <ilanvol96@gmail.com>
2026-03-28 22:51:11 +0300

    change name

---
+++ Space Scrapper/Assets/Scripts/GameStartMenu.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class StartMenuUI : MonoBehaviour
+{
+    private enum SubMenu { Main, Options, About, None }
+
+    [Header("UI Pages")]
+    [SerializeField] private GameObject mainMenuPage;
+    [SerializeField] private GameObject optionsPage;
+    [SerializeField] private GameObject aboutPage;
+
+    [Header("Buttons")]
+    [SerializeField] private Button startButton;
+    [SerializeField] private Button optionsButton;
+    [SerializeField] private Button aboutButton;
+    [SerializeField] private Button quitButton;
+    [SerializeField] private List<Button> returnButtons;
+
+    private void Awake()
+    {
+        startButton.onClick.AddListener(() => {
+            ShowPage(SubMenu.None); // Hide UI for transition
+            SceneTransitionUI.Instance.FadeOut(() => {
+                SceneLoader.Load(SceneLoader.Scene.MainSpaceshipScene);
+            });
+        });
+
+        optionsButton.onClick.AddListener(() => ShowPage(SubMenu.Options));
+        aboutButton.onClick.AddListener(() => ShowPage(SubMenu.About));
+        quitButton.onClick.AddListener(Application.Quit);
+
+        foreach (var btn in returnButtons)
+        {
+            btn.onClick.AddListener(() => ShowPage(SubMenu.Main));
+        }
+    }
+
+    private void Start()
+    {
+        ShowPage(SubMenu.Main);
+    }
+
+    private void ShowPage(SubMenu targetPage)
+    {
+        mainMenuPage.SetActive(targetPage == SubMenu.Main);
+        optionsPage.SetActive(targetPage == SubMenu.Options);
+        aboutPage.SetActive(targetPage == SubMenu.About);
+    }
+}
