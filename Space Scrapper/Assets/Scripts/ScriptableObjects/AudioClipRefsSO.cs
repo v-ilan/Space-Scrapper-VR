c9bb17f088d89d5c709ba21c3b0ac561a1d61a94
v-ilan <ilanvol96@gmail.com>
2026-03-28 17:06:52 +0300

    Update AudioClipRefsSO.cs

--- Space Scrapper/Assets/Scripts/ScriptableObjects/AudioClipRefsSO.cs
+++ Space Scrapper/Assets/Scripts/ScriptableObjects/AudioClipRefsSO.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 [CreateAssetMenu(fileName = "AudioClipRefsSO", menuName = "Scriptable Objects/AudioClipRefsSO")]
 public class AudioClipRefsSO : ScriptableObject
