6969c9b75cacd934ff8b4eac4c7fe9a18fd73af4
v-ilan <ilanvol96@gmail.com>
2026-03-28 23:21:13 +0300

    Update OptionsMenuUI.cs

--- Space Scrapper/Assets/Scripts/UI/OptionsMenuUI.cs
+++ Space Scrapper/Assets/Scripts/UI/OptionsMenuUI.cs
@@ -1,16 +1,67 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using TMPro;
+using System;
 
 public class OptionsMenuUI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private enum SubMenu { Options, Locomotion, None }
+
+    [Header("UI Pages")]
+    [SerializeField] private GameObject mainMenuPage;
+    [SerializeField] private GameObject locomotionPage;
+
+    [Header("Buttons")]
+    [SerializeField] private Button locomotionButton;
+    [SerializeField] private List<Button> returnButtons;
+
+    [Header("Sound")]
+    private TextMeshPro musicText;
+    private Slider musicSlider;
+    private TextMeshPro sfxText;
+    private Slider sfxSlider;
+
+    private void Awake()
     {
-        
+        InitButtons();
+        Initsounds();
     }
 
-    // Update is called once per frame
-    void Update()
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
     {
+        ShowPage(SubMenu.Options);
+    }
+
+    private void InitButtons()
+    {
+        locomotionButton.onClick.AddListener(() => ShowPage(SubMenu.Locomotion));
         
+        foreach (var btn in returnButtons)
+        {
+            btn.onClick.AddListener(() => ShowPage(SubMenu.Options));
+        }
+    }
+
+    private void Initsounds()
+    {
+        musicSlider.onValueChanged.AddListener((value) => {
+            MusicManager.Instance.ChangeVolume(value);
+            UpdateVisuals();
+            });
+    }
+
+    private void ShowPage(SubMenu targetPage)
+    {
+        mainMenuPage.SetActive(targetPage == SubMenu.Options);
+        locomotionPage.SetActive(targetPage == SubMenu.Locomotion);
+    }
+
+    private void UpdateVisuals()
+    {
+        musicText.text = "Music: " + Mathf.RoundToInt(MusicManager.Instance.GetVolume() * 10f);
+        sfxText.text = "SFX: " + Mathf.RoundToInt(SoundManager.Instance.GetVolume() * 10f);
     }
 }
