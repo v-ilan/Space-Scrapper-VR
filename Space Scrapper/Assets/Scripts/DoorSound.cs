ddb68b7593dc2184a63071631d8e09a48c069872
v-ilan <ilanvol96@gmail.com>
2026-03-28 22:36:11 +0300

    Update DoorSound.cs

--- Space Scrapper/Assets/Scripts/DoorSound.cs
+++ Space Scrapper/Assets/Scripts/DoorSound.cs
@@ -1,16 +1,45 @@
+using System;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class DoorSound : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private OpenDoor openDoor;
+    private AudioSource audioSource;
+
+    private void Awake()
     {
+        if (openDoor == null) 
+        {
+            openDoor = GetComponentInParent<OpenDoor>();
+        }
+        audioSource = GetComponent<AudioSource>();
         
+        // 3D spatialization
+        audioSource.spatialBlend = 1.0f; 
+        audioSource.playOnAwake = false;
+        audioSource.loop = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnEnable()
     {
-        
+        openDoor.OnDoorToggle += DoorController_OnDoorStateChanged;
+
+        SoundManager.Instance.OnVolumeChange += SoundManager_OnVolumeChange;
+        UpdateVolume();
     }
+
+    void OnDisable()
+    {
+        openDoor.OnDoorToggle -= DoorController_OnDoorStateChanged;
+        SoundManager.Instance.OnVolumeChange -= SoundManager_OnVolumeChange;
+    }
+
+    private void DoorController_OnDoorStateChanged(object sender, EventArgs e)
+    {
+        audioSource.PlayOneShot(audioSource.clip);
+    }
+
+    private void SoundManager_OnVolumeChange(object sender, System.EventArgs e) => UpdateVolume();
+    private void UpdateVolume() => audioSource.volume = SoundManager.Instance.GetVolume();
 }
